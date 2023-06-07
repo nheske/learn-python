ee368ec603870e01ad075c8f9c7b04a65f9a04f4
nheske <you@example.com>
2023-06-07 10:20:59 -0700

    add STL

---
+++ chatgpt/stl/cylinder.py
@@ -0,0 +1,20 @@
+##prompt: write python program to generate a cylinder 6 in long and 1 inch in radius using pyvista library and saved to stl file and also visually renders it
+# can render STL file in https://3dviewer.net/
+#pip install pyvista
+import pyvista as pv
+
+# Dimensions in inches
+radius = 1.0
+height = 6.0
+file_path = "cylinder.stl"
+
+# Generate cylinder
+cylinder = pv.Cylinder(center=(0, 0, 0), direction=(0, 0, 1), radius=radius, height=height)
+
+# Save cylinder as STL file
+cylinder.save(file_path)
+
+# Visualize the cylinder
+plotter = pv.Plotter()
+plotter.add_mesh(cylinder, color='white')
+plotter.show()
