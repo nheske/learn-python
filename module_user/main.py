93cbcd86fcf68cbe8a55d2829543133cdcb0312a
nheske <you@example.com>
2023-06-07 08:59:03 -0700

    add chatgpt folder

--- module_user/main.py
+++ module_user/main.py
@@ -1,4 +1,7 @@
-from module_folder.module_file import say_hello
+#from ..module_folder import module_file.
+import module_folder.module_file
+#from ..module_folder.module_file import say_hello
 
+print(module_file.say_hello)
 print("Hello World")
-print(say_hello())
+#print(say_hello())
